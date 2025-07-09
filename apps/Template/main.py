2456d08a0c487045ebb0c8968229735517f3e4b5
Matthew20325 <pokemonwolf2520@gmail.com>
2025-07-08 22:36:56 -0400

    first app test

---
+++ apps/Template/main.py
@@ -0,0 +1,8 @@
+import customtkinter as ctk
+from tkinter import colorchooser
+
+APP_WIDTH = 400
+APP_HEIGHT = 400
+
+def launch_app(parent):
+    print("This is a template app. Replace this code with your own app logic.")
